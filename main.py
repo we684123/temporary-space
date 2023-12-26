61b95ece2be98b9d0e730f476f387ffeb6201acb
we684123 <we684123@gmail.com>
2023-12-26 10:15:16 +0800

    ✨ 新增 下載公開檔案的功能

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, send_from_directory
 from pathlib import Path
 
 app = Flask(__name__)
 UPLOAD_FOLDER = Path('./files')
+PUBLIC_FOLDER = Path('./public')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['PUBLIC_FOLDER'] = PUBLIC_FOLDER
 
-# 確保上傳目錄存在
+# 確保上傳目錄和公開目錄存在
 UPLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
+PUBLIC_FOLDER.mkdir(parents=True, exist_ok=True)
+
+@app.route('/list_files')
+def list_files():
+    files = [f.name for f in PUBLIC_FOLDER.iterdir() if f.is_file()]
+    return render_template_string('''
+        <!doctype html>
+        <title>檔案列表</title>
+        <h1>可下載的檔案</h1>
+        <ul>
+            {% for file in files %}
+                <li><a href="{{ url_for('download_file', filename=file) }}">{{ file }}</a></li>
+            {% endfor %}
+        </ul>
+        <a href="/">返回上傳頁面</a>
+    ''', files=files)
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['PUBLIC_FOLDER'], filename)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -39,6 +61,7 @@ def upload_file():
       <input type=file name=file multiple>
       <input type=submit value=上傳>
     </form>
+    <a href="/list_files">瀏覽可下載檔案</a>
     '''
 
 if __name__ == '__main__':
